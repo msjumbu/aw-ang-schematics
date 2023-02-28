b14a708e339357ca406578e431ecc037827f6995
Senthilkumar <senthilkumar@gmail.com>
2023-02-28 07:33:04 +0530

    Added conditional prompt for otds url

--- src/authenticate/index.ts
+++ src/authenticate/index.ts
@@ -3,12 +3,27 @@ import { apply, applyTemplates, chain, MergeStrategy, mergeWith, move, Rule, Sch
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import { setConfig } from '../utils/util';
 import { ConfigSchema as OTDSAuthSchema } from './schema';
+import * as inquirer from "inquirer";
 
 export function authentication(options: OTDSAuthSchema): Rule {
   return async (tree: Tree, _context: SchematicContext) => {
+    const questions = [
+      {
+        type: 'input',
+        name: 'otdsurl',
+        message: "Please enter your OTDS authentication rest service URL.",
+      }];
     if (!options.auth_type) options.auth_type = 'OTDS';
-    if (options.auth_type == 'OTDS' && !options.otds_url)
-      throw new SchematicsException('OTDS URL is required for OTDS authentication');
+
+    if (options.auth_type == 'OTDS' && !options.otds_url) {
+      if (isTTY()) {
+        const prompt = inquirer.createPromptModule();
+        let t = await prompt(questions);
+        options.otds_url = t['otdsurl'];
+      }
+      if (!options.otds_url)
+        throw new SchematicsException('OTDS URL is required for OTDS authentication');
+    }
 
     const workspace = await getWorkspace(tree);
     const project = (options.project != null) ? workspace.projects.get(options.project) : null;
@@ -34,4 +49,19 @@ export function authentication(options: OTDSAuthSchema): Rule {
     return chain([
       mergeWith(templateSource, MergeStrategy.Overwrite)]);
   };
+}
+
+function _isTruthy(value: undefined | string): boolean {
+  // Returns true if value is a string that is anything but 0 or false.
+  return value !== undefined && value !== '0' && value.toUpperCase() !== 'FALSE';
+}
+
+function isTTY(): boolean {
+  // If we force TTY, we always return true.
+  const force = process.env['NG_FORCE_TTY'];
+  if (force !== undefined) {
+    return _isTruthy(force);
+  }
+
+  return !!process.stdout.isTTY && !_isTruthy(process.env['CI']);
 }
--- src/authenticate/index_spec.ts
+++ src/authenticate/index_spec.ts
@@ -7,72 +7,70 @@ import { readConfig } from '../utils/util';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
-describe('OTDS Authentication', () => {
+const testRunner = new SchematicTestRunner(
+  'rocket',
+  require.resolve(collectionPath),
+);
+const schematicRunner = new SchematicTestRunner(
+  'msjumbu', collectionPath);
+const projectName = 'bar';
 
-  const testRunner = new SchematicTestRunner(
-    'rocket',
-    require.resolve(collectionPath),
-  );
-  const schematicRunner = new SchematicTestRunner(
-    'msjumbu', collectionPath);
-  const projectName = 'bar';
-  describe('OTDS Authentication', () => {
-    const awOptions: AWSchema = {
-      gateway_url: 'test',
-      org_dn: '',
-      config_path: '',
-      project: 'bar',
+describe('OTDS Authentication', () => {
+  const awOptions: AWSchema = {
+    gateway_url: 'test',
+    org_dn: '',
+    config_path: '',
+    project: 'bar',
+    auth_type: 'OTDS',
+    otds_url: 'rest',
+  };
+
+  let appTree: UnitTestTree;
+  beforeEach(async () => {
+    appTree = await createTestApp(projectName, testRunner);
+    appTree = await schematicRunner.runSchematic('app-works', awOptions, appTree);
+  });
+  it('should configure OTDS support', async () => {
+    const otdsOptions: MyConfigSchema = {
       auth_type: 'OTDS',
       otds_url: 'rest',
+      project: projectName
     };
-
-    let appTree: UnitTestTree;
-    beforeEach(async () => {
-      appTree = await createTestApp(projectName, testRunner);
-      appTree = await schematicRunner.runSchematic('app-works', awOptions, appTree);
-    });
-    it('should configure OTDS support', async () => {
-      const otdsOptions: MyConfigSchema = {
-        auth_type: 'OTDS',
-        otds_url: 'rest',
-        project: projectName
-      };
-      // const runner = new SchematicTestRunner('schematics', collectionPath);
-      const tree = await schematicRunner.runSchematic('authenticate', otdsOptions, appTree);
-      const files = tree.files;
-      expect(files).toContain('/projects/bar/src/app/services/authentication.service.ts');
-      let auth_type = readConfig(tree, '/projects/bar/src', 'AUTH_TYPE');
-      expect(auth_type).toEqual('OTDS');
-      let otds_url = readConfig(tree, '/projects/bar/src', 'OTDS_URL');
-      expect(otds_url).toEqual('rest');
-    });
+    // const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await schematicRunner.runSchematic('authenticate', otdsOptions, appTree);
+    const files = tree.files;
+    expect(files).toContain('/projects/bar/src/app/services/authentication.service.ts');
+    let auth_type = readConfig(tree, '/projects/bar/src', 'AUTH_TYPE');
+    expect(auth_type).toEqual('OTDS');
+    let otds_url = readConfig(tree, '/projects/bar/src', 'OTDS_URL');
+    expect(otds_url).toEqual('rest');
   });
+});
 
-  describe('AW Authentication', () => {
-    const awOptions: AWSchema = {
-      gateway_url: 'test',
-      org_dn: '',
-      config_path: '',
-      project: 'bar',
+describe('AW Authentication', () => {
+  const awOptions: AWSchema = {
+    gateway_url: 'test',
+    org_dn: '',
+    config_path: '',
+    project: 'bar',
+    auth_type: 'AW',
+  };
+
+  let appTree: UnitTestTree;
+  beforeEach(async () => {
+    appTree = await createTestApp(projectName, testRunner);
+    appTree = await schematicRunner.runSchematic('app-works', awOptions, appTree);
+  });
+  it('should configure AW', async () => {
+    const otdsOptions: MyConfigSchema = {
       auth_type: 'AW',
+      project: projectName
     };
-
-    let appTree: UnitTestTree;
-    beforeEach(async () => {
-      appTree = await createTestApp(projectName, testRunner);
-      appTree = await schematicRunner.runSchematic('app-works', awOptions, appTree);
-    });
-    it('should configure AW', async () => {
-      const otdsOptions: MyConfigSchema = {
-        auth_type: 'AW',
-        project: projectName
-      };
-      // const runner = new SchematicTestRunner('schematics', collectionPath);
-      const tree = await schematicRunner.runSchematic('authenticate', otdsOptions, appTree);
-      const files = tree.files;
-      expect(files).toContain('/projects/bar/src/app/services/authentication.service.ts');
-      let auth_type = readConfig(tree, '/projects/bar/src', 'AUTH_TYPE')
-      expect(auth_type).toEqual('AW');
-    });
+    // const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await schematicRunner.runSchematic('authenticate', otdsOptions, appTree);
+    const files = tree.files;
+    expect(files).toContain('/projects/bar/src/app/services/authentication.service.ts');
+    let auth_type = readConfig(tree, '/projects/bar/src', 'AUTH_TYPE')
+    expect(auth_type).toEqual('AW');
   });
 });
